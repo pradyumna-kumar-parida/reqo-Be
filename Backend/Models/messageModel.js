5e6b8fd841f89833b51d60a2f0735a655b84de67
Uma Shankar <cashdhell@gmail.com>
2026-02-12 19:18:31 +0530

    chatpage added

---
+++ Backend/Models/messageModel.js
@@ -0,0 +1,27 @@
+import mongoose, { Types } from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    read: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+export const messageModel = mongoose.model("Message", messageSchema);
